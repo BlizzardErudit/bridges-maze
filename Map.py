419dd9246a1272c082f5bc9a4787ed444e16e6eb
musdx <blue.neutron460@slmail.me>
2026-05-02 13:16:42 -0400

    feat: direction detection for pixel

--- Map.py
+++ Map.py
@@ -8,8 +8,41 @@ class Pixel:
         self.south= south
         self.east = east
         self.west = west
+    def available_water_direction():
+        available = []
+        if self.nord == State.State.Water:
+            available.append("nord")
+        elif self.south == State.State.Water:
+            available.append("south")
+        elif self.east == State.State.Water:
+            available.append("east")
+        elif self.west == State.State.Water:
+            available.append("west")
+        return available
+    def available_bridge_direction():
+        available = []
+        if self.nord == State.State.Bridge:
+            available.append("nord")
+        elif self.south == State.State.Bridge:
+            available.append("south")
+        elif self.east == State.State.Bridge:
+            available.append("east")
+        elif self.west == State.State.Bridge:
+            available.append("west")
+        return available
+    def turn_direction_brigde(name: str):
+        match name:
+            case "nord":
+                self.nord = State.State.Bridge
+            case "south":
+                self.south = State.State.Bridge
+            case "east":
+                self.east = State.State.Bridge
+            case "weast":
+                self.weast = State.State.Bridge
+    
 
-def empty_map(height: int, width: int):
+def empty_map(height: int, width: int) -> list[list[Pixel]]:
     the_map = []
     for _ in range(height):
         j = []
@@ -37,13 +70,18 @@ def empty_map(height: int, width: int):
         elif i == 49:
             nord_line.append(Pixel(State.State.Bridge, State.State.Water, State.State.NordBorder, State.State.Water,State.State.Bridge))
         else :
-            nord_line.append(Pixel(State.State.Bridge, State.State.Water, State.State.NordBorder, State.State.Bridge,State.State.Bridge))
+            nord_line.append(Pixel(State.State.Bridge, State.State.Water, State.State.SouthBorder, State.State.Bridge,State.State.Bridge))
     the_map.append(nord_line)
     return the_map
 
 def init_map(height: int, width: int):
-    the_map = empty_map(height, width)
-    
+    direction = []
+    the_map = empty_map
+    for _ in range(random.randint(10, 15)):
+        first_pixel_y = random.randint(50)
+        first_pixel_x = the_map.len()-1
+        first_pixel = the_map[-1][first_pixel]
+        ran = random.choice(first_pixel.available_water_direction())
 
 
 if __name__ == "__main__":
