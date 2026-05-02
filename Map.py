38ae445cf1f638d80f132bf15903767ca684c139
musdx <blue.neutron460@slmail.me>
2026-05-02 14:12:03 -0400

    feat: directional pixel

--- Map.py
+++ Map.py
@@ -2,33 +2,36 @@ import State
 import random
 
 class Pixel:
-    def __init__(self, property: State.State=State.State.Water, north: State.State=State.State.Water, south: State.State=State.State.Water, east: State.State=State.State.Water, west: State.State=State.State.Water):
+    def __init__(self, property: State.State=State.State.Water, north: State.State=State.State.Water, south: State.State=State.State.Water, east: State.State=State.State.Water, west: State.State=State.State.Water, x: int=0, y: int=0):
         self.property = property
         self.north = north
         self.south= south
         self.east = east
         self.west = west
+        self.x = x
+        self.y = y
+
     def available_water_direction(self):
         available = []
         if self.north == State.State.Water:
-            available.append("north")
+            available.append((self.y+1,self.x,"north"))
         if self.south == State.State.Water:
-            available.append("south")
+            available.append((self.y-1,self.x, "south"))
         if self.east == State.State.Water:
-            available.append("east")
+            available.append((self.y,self.x+1, "east"))
         if self.west == State.State.Water:
-            available.append("west")
+            available.append((self.y,self.x-1,"west"))
         return available
     def available_bridge_direction(self):
         available = []
         if self.north == State.State.Bridge:
-            available.append("north")
+            available.append((self.y+1,self.x))
         if self.south == State.State.Bridge:
-            available.append("south")
+            available.append((self.y-1,self.x))
         if self.east == State.State.Bridge:
-            available.append("east")
+            available.append((self.y,self.x+1))
         if self.west == State.State.Bridge:
-            available.append("west")
+            available.append((self.y,self.x-1))
         return available
     def turn_direction_brigde(self, name: str):
         match name:
@@ -44,34 +47,34 @@ class Pixel:
 
 def empty_map(height: int, width: int) -> list[list[Pixel]]:
     the_map = []
-    for _ in range(height):
+    for a in range(height+2):
         j = []
-        for _ in range(width):
-            j.append(Pixel())
+        if a == 1:
+            for i in range(width):
+                j.append(Pixel(north=State.State.Bridge,x=i,y=a))
+        elif a==height:
+            for i in range(width):
+                j.append(Pixel(south=State.State.Bridge,x=i,y=a))
+        elif a == 0:
+            for i in range(width):
+                if i == 0:
+                    j.append(Pixel(State.State.Bridge, State.State.Water, State.State.NorthBorder, State.State.Bridge,State.State.Water,i,a))
+                elif i == 49:
+                    j.append(Pixel(State.State.Bridge, State.State.Water, State.State.NorthBorder, State.State.Water,State.State.Bridge,i,a))
+                else :
+                    j.append(Pixel(State.State.Bridge, State.State.Water, State.State.NorthBorder, State.State.Bridge,State.State.Bridge,i,a))
+        elif a == height+1:
+            for i in range(width):
+                if i == 0:
+                    j.append(Pixel(State.State.Bridge, State.State.Water, State.State.SouthBorder, State.State.Bridge,State.State.Water,i,a))
+                elif i == 49:
+                    j.append(Pixel(State.State.Bridge, State.State.Water, State.State.SouthBorder, State.State.Water,State.State.Bridge,i,a))
+                else :
+                    j.append(Pixel(State.State.Bridge, State.State.Water, State.State.SouthBorder, State.State.Bridge,State.State.Bridge,i,a))
+        else:
+            for i in range(width):
+                j.append(Pixel(x=i,y=a))
         the_map.append(j)
-
-    south_line = []
-    for i in range(width):
-        if i == 0:
-            south_line.append(Pixel(State.State.Bridge, State.State.Water, State.State.SouthBorder, State.State.Bridge,State.State.Water))
-        elif i == 49:
-            south_line.append(Pixel(State.State.Bridge, State.State.Water, State.State.SouthBorder, State.State.Water,State.State.Bridge))
-        else :
-            south_line.append(Pixel(State.State.Bridge, State.State.Water, State.State.SouthBorder, State.State.Bridge,State.State.Bridge))
-    the_map.reverse()
-    the_map.append(south_line)
-    the_map.reverse()
-
-
-    north_line = []
-    for i in range(width):
-        if i == 0:
-            north_line.append(Pixel(State.State.Bridge, State.State.Water, State.State.northBorder, State.State.Bridge,State.State.Water))
-        elif i == 49:
-            north_line.append(Pixel(State.State.Bridge, State.State.Water, State.State.northBorder, State.State.Water,State.State.Bridge))
-        else :
-            north_line.append(Pixel(State.State.Bridge, State.State.Water, State.State.SouthBorder, State.State.Bridge,State.State.Bridge))
-    the_map.append(north_line)
     return the_map
 
 def init_map(height: int, width: int):
@@ -82,6 +85,8 @@ def init_map(height: int, width: int):
         first_pixel_x = the_map.len()-1
         first_pixel = the_map[-1][first_pixel]
         ran = random.choice(first_pixel.available_water_direction())
+        
+        # current_pixel = 
 
 
 if __name__ == "__main__":
--- State.py
+++ State.py
@@ -2,6 +2,6 @@ from enum import Enum
 
 class State(Enum):
   Water = 1
-  NordBorder = 2
+  NorthBorder = 2
   SouthBorder = 3 
   Bridge = 4
